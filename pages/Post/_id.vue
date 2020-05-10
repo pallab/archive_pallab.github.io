86a589133a7d3862caee7345a81e958449e9fcaa
pallab <pallab@xyz.com>
2020-05-10 14:00:54 +0530

    wip

--- pages/Post/_id.vue
+++ pages/Post/_id.vue
@@ -1,26 +1,8 @@
 <template>
-  <div class="card">
-    <header class="card-header">
-      <p class="card-header-title">{{ post.title }}</p>
-    </header>
-    <div class="card-content">
-      <p class="title">{{ post.body }}</p>
-      <p class="subtitle">{{ post.summary }}</p>
-    </div>
-    <footer class="card-footer">
-      <p class="card-footer-item">
-        <span>
-          View on
-          <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a>
-        </span>
-      </p>
-      <p class="card-footer-item">
-        <span>
-          Share on
-          <a href="#">Facebook</a>
-        </span>
-      </p>
-    </footer>
+  <div class="content post">
+    <h1 class="post-item">{{ post.title }}</h1>
+    <h6 class="post-item">{{ post.date }}</h6>
+    <p class="post-item">{{ post.body }}</p>
   </div>
 </template>
 
@@ -39,26 +21,34 @@ interface Post {
 export default Vue.extend({
   name: "Post",
 
-  // props: {
-  //   id: Number
+  // data() {
+  //   return {
+  //     id: this.$route.params.id
+  //   };
   // },
-
-  data() {
-    return {
-      id: this.$route.params.id
-    };
-  },
 
   computed: {
     post() {
-       const id = parseInt(this.$route.params.id);
-      return postsJson.find( post => post.id === id );
-      // return postsJson.find(function(post, index) {
-      //   return post.id === id ? true : false;
-      // });
+      const id = parseInt(this.$route.params.id);
+      return postsJson.find(post => post.id === id);
     }
   }
 });
 </script>
 
-<style lang="stylus" scoped></style>
+<style scoped>
+.post {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+}
+
+.post-item {
+  max-width: 1200px;
+  text-align: center;
+  padding-left: 30px;
+  padding-right: 30px;
+}
+</style>
