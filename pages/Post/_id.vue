42192ee98db6c93ac870beeb424e795b5a0a8e91
pallab <pallab@xyz.com>
2020-05-10 23:06:41 +0530

    wip

--- pages/Post/_id.vue
+++ pages/Post/_id.vue
@@ -22,7 +22,7 @@ export default Vue.extend({
   },
 
   async asyncData ({ $axios , params}) {
-    let data = await $axios.$get(`/articles/${params.id}.json`);
+    let data = await $axios.$get(`/articles/${params.id}.html`);
     return { postBody: data }
   },
 
